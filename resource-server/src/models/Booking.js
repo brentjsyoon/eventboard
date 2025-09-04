30bd524ace8717886540df5fa0efe08d1b4eac99
brentjsyoon <brentyoon10@gmail.com>
2025-09-04 13:56:16 -0400

    Bug fix in resource-server Booking schema

--- resource-server/src/models/Booking.js
+++ resource-server/src/models/Booking.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
-    event: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
-    user: { type: String, required: true }, // can also store user ID/email
-    seats: { type: Number, default: 1 }, // optional
-}, { timestamps: true });
+  event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
+  user: String,
+  seats: Number,
+});
 
-export default mongoose.model("Booking", bookingSchema);
+module.exports = mongoose.model("Booking", bookingSchema);
