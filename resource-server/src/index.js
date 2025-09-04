8818c45900c718b16bef5797cbb7cfb7a2b42553
brentjsyoon <brentyoon10@gmail.com>
2025-09-04 16:26:15 -0400

    Deployment debugging

--- resource-server/src/index.js
+++ resource-server/src/index.js
@@ -11,6 +11,8 @@ const app = express();
 
 connectDB();
 
+console.log("CLIENT_URL:", process.env.CLIENT_URL);
+
 // --- CORS Middleware ---
 app.use(cors({
    origin: process.env.CLIENT_URL,        // must match your frontend exactly
@@ -26,8 +28,6 @@ app.options("*", cors({
   allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
-
-console.log("CLIENT_URL:", process.env.CLIENT_URL);
 
 app.use(express.json());
 
